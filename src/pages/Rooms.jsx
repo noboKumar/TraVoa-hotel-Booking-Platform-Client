31c2776e92a75efaaa7d8fc740f7c892097e4c0a
noboKumar <nobokumar227@gmail.com>
2025-06-10 01:23:46 +0600

    add loading spinner to rooms data

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -4,22 +4,33 @@ import { fetchRoomData } from "../API/roomsApi";
 import RoomsCards from "../components/RoomsCards";
 import PageTitle from "../components/PageTitle";
 import { MdLocalHotel } from "react-icons/md";
+import useAuth from "../hooks/useAuth";
+import Loading from "../components/Loading";
 
 const Rooms = () => {
+  const { loading, setLoading } = useAuth();
   const [roomData, setRoomData] = useState([]);
+
   useEffect(() => {
     fetchRoomData()
       .then((data) => {
         setRoomData(data);
       })
-      .catch((err) => console.log(err));
-  }, []);
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [setLoading]);
+  
+  if (loading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <Helmet>
         <title>TraVoa | Rooms</title>
       </Helmet>
-      <PageTitle title={"Rooms"} logo={<MdLocalHotel  />}></PageTitle>
+      <PageTitle title={"Rooms"} logo={<MdLocalHotel />}></PageTitle>
       <div className="grid grid-cols-3 gap-10">
         {roomData.map((data) => (
           <RoomsCards key={data._id} data={data}></RoomsCards>
