6e75ef386ffa6c8a383771eb1c7f83caa280fa5f
noboKumar <nobokumar227@gmail.com>
2025-07-02 18:21:32 +0600

    change server url

--- src/API/apiClient.js
+++ src/API/apiClient.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  // baseURL: "https://ph-assignment-11-server-ebon.vercel.app",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://ph-assignment-11-server-ebon.vercel.app",
 });
