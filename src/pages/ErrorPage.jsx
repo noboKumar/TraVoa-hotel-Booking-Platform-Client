5c69cb9243a69efa7e678419417e9315f9bd801a
noboKumar <nobokumar227@gmail.com>
2025-08-09 14:03:32 +0600

    update error page animation

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import errorAnimation from "../assets/errorAnimation.json";
+import errorAnimation from "../assets/404 Error.json";
 import Lottie from "lottie-react";
 import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="w-3/10 mx-auto text-center">
-      <Lottie animationData={errorAnimation}></Lottie>
-      <Link to={"/"} className="btn btn-primary">Back to Home</Link>
+    <div className="w-3/10 py-20 mx-auto text-center">
+      <div>
+        <Lottie animationData={errorAnimation}></Lottie>
+        <h1 className="py-5 md:text-5xl font-bold">Error Not Found</h1>
+      </div>
+      <Link to={"/"} className="btn btn-primary">
+        Back to Home
+      </Link>
     </div>
   );
 };
